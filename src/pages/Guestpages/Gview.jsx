ecc47c6812ac878bb1d4f8cd454048aadf6a5633
kiddo9 <dkido913@gmail.com>
2025-01-26 00:08:20 +0100

    another update

--- src/pages/Guestpages/Gview.jsx
+++ src/pages/Guestpages/Gview.jsx
@@ -77,25 +77,31 @@ const Gview = () => {
 
             <div className="flex flex-col lg:grid lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-9 mt-3">
                {
+               dish == null || dish.length == 0 ? (
+                <div>
+                    <p className="text-center py-10 text-xl font-bold">no dish added yet</p>
+                </div>
+               ): (
                 dish.map((food) => (
                     
-                <div key={food.id} className="flex items-center justify-around lg:w-full rounded-2xl shadow-2xl mx-3 py-2 px-1">
-                    <img src={food.image} className="w-32 h-32" alt="" />
-                    <div>
-                        <p className="text-center">{food.name}</p>
-                        <div className="flex justify-between w-32  items-center">
-                            <span className="border cursor-pointer w-5 h-5 flex justify-center items-center border-black rounded-full">-</span>
-                            <span className="cursor-not-allowed">8</span>
-                            <span className="border cursor-pointer w-5 h-5 flex justify-center items-center border-black rounded-full">+</span>
+                    <div key={food.id} className="flex items-center justify-around lg:w-full rounded-2xl shadow-2xl mx-3 py-2 px-1">
+                        <img src={food.image} className="w-32 h-32" alt="" />
+                        <div>
+                            <p className="text-center">{food.name}</p>
+                            <div className="flex justify-between w-32  items-center">
+                                <span className="border cursor-pointer w-5 h-5 flex justify-center items-center border-black rounded-full">-</span>
+                                <span className="cursor-not-allowed">8</span>
+                                <span className="border cursor-pointer w-5 h-5 flex justify-center items-center border-black rounded-full">+</span>
+                            </div>
+                            <p className="text-center">{'$'} {food.price}</p>
                         </div>
-                        <p className="text-center">{'$'} {food.price}</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-red-500">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-red-500">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
-                </div>
-                    
-                ))
+                        
+                    ))
+               )
                 
                 }  
             </div>
